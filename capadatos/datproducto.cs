fffafcef0683bd5b52c0f446c7a3579bbd12b5ac
James <yrondon@gmail.com>
2023-11-17 14:58:46 -0500

    XD

--- capadatos/datproducto.cs
+++ capadatos/datproducto.cs
@@ -22,56 +22,19 @@ namespace capadatos
                 return datproducto._instancia;
             }
         }
-
-        ////////////////////listado de Clientes
-        public List<entproducto> ListarCliente()
-        {
-            SqlCommand cmd = null;
-            List<entproducto> lista = new List<entproducto>();
-            try
-            {
-                SqlConnection cn = conexion.Instancia.Conectar(); //singleton
-                cmd = new SqlCommand("spListarCliente", cn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cn.Open();
-                SqlDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
-                {
-                    entproducto pro = new entproducto();
-                    pro.idproducto = Convert.ToInt32(dr["idproducto"]);
-                    pro.nombre = dr["nombre"].ToString();
-                    pro.descripcion = dr["descripcion"].ToString();
-                    pro.precio = Convert.ToDecimal(dr["precion"]);
-                    pro.estado = Convert.ToBoolean(dr["estado"]);
-                    pro.rubro = dr["rubro"].ToString();
-                    lista.Add(pro);
-                }
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
- finally
- {
- cmd.Connection.Close();
- }
-return lista;
- }
- /////////////////////////InsertaCliente
- public Boolean InsertarCliente(entproducto pro)
+ /////////////////////////InsertaProducto
+ public Boolean InsertarProducto(entproducto pro)
 {
     SqlCommand cmd = null;
     Boolean inserta = false;
     try
     {
         SqlConnection cn = conexion.Instancia.Conectar();
-        cmd = new SqlCommand("spInsertarCliente", cn);
+        cmd = new SqlCommand("spInsertarProducto", cn);
         cmd.CommandType = CommandType.StoredProcedure;
         cmd.Parameters.AddWithValue("@nombre", pro.nombre);
-        cmd.Parameters.AddWithValue("@descripcion", pro.descripcion);
         cmd.Parameters.AddWithValue("@precio", pro.precio);
         cmd.Parameters.AddWithValue("@estado", pro.estado);
-        cmd.Parameters.AddWithValue("@rubro", pro.rubro);
         cn.Open();
         int i = cmd.ExecuteNonQuery();
         if (i > 0)
@@ -86,62 +49,5 @@ return lista;
     finally { cmd.Connection.Close(); }
     return inserta;
 }
-//////////////////////////////////EditaCliente
-public Boolean EditarCliente(entproducto pro)
-{
-    SqlCommand cmd = null;
-    Boolean edita = false;
-    try
-    {
-        SqlConnection cn = conexion.Instancia.Conectar();
-        cmd = new SqlCommand("spEditarCliente", cn);
-        cmd.CommandType = CommandType.StoredProcedure;
-        cmd.Parameters.AddWithValue("@nombre", pro.nombre);
-        cmd.Parameters.AddWithValue("@descripcion", pro.descripcion);
-        cmd.Parameters.AddWithValue("@precio", pro.precio);
-        cmd.Parameters.AddWithValue("@estado", pro.estado);
-        cmd.Parameters.AddWithValue("@rubro", pro.rubro);
- 
- cn.Open();
- int i = cmd.ExecuteNonQuery();
- if (i > 0)
- {
- edita = true;
- }
-}
- catch (Exception e)
-{
-    throw e;
-}
-finally { cmd.Connection.Close(); }
-return edita;
- }
- //deshabilitaCliente
- public Boolean DeshabilitarCliente(entproducto pro)
-{
-    SqlCommand cmd = null;
-    Boolean delete = false;
-    try
-    {
-        SqlConnection cn = conexion.Instancia.Conectar();
-        cmd = new SqlCommand("spDesabilitarCliente", cn);
-        cmd.CommandType = CommandType.StoredProcedure;
-        cmd.Parameters.AddWithValue("@idproducto", pro.idproducto );
-        cmd.Parameters.AddWithValue("@estado", pro.estado);
-        cn.Open();
-        int i = cmd.ExecuteNonQuery();
-        if (i > 0)
-        {
-            delete = true;
-        }
-    }
-    catch (Exception e)
-    {
-        throw e;
-    }
-    finally { cmd.Connection.Close(); }
-    return delete;
-}
- }
-    }
+
 
